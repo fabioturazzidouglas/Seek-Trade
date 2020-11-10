3082289f9745442648e816c6dedde10274d8d9ff
denngall <volovikd@student.douglascollege.ca>
2020-11-10 12:50:59 -0800

    Added user registration and logout button to the main activity (now for testing).

--- app/src/main/java/com/example/seektradeapp/Register.java
+++ app/src/main/java/com/example/seektradeapp/Register.java
@@ -1,13 +1,21 @@
 package com.example.seektradeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
@@ -32,5 +40,48 @@ public class Register extends AppCompatActivity {
 
         fAuth = FirebaseAuth.getInstance();
         progBar = findViewById(R.id.progressBar);
+
+        if(fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nEmail = email.getText().toString().trim();
+                String password = pass.getText().toString();
+
+                if(TextUtils.isEmpty(nEmail)) {
+                    email.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)) {
+                    pass.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length() < 6) {
+                    pass.setError("Password Must have more than 6 Characters");
+                }
+
+                progBar.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(nEmail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Register.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+            }
+        });
     }
 }
