64b4414a61c68f29dc9af26263be9dacdb9e2414
fabioturazzidouglas <cortespinheiroturazzif@student.douglascollege.ca>
2020-11-14 16:16:17 -0800

    Added photo upload when creating a post

--- app/src/main/java/com/example/seektradeapp/CreatePostActivity.java
+++ app/src/main/java/com/example/seektradeapp/CreatePostActivity.java
@@ -1,15 +1,33 @@
 package com.example.seektradeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class CreatePostActivity extends AppCompatActivity {
     int LAUNCH_SECOND_ACTIVITY = 1;
@@ -21,6 +39,16 @@ public class CreatePostActivity extends AppCompatActivity {
     EditText postZip;
     Spinner category;
     Button previewBtn;
+    //Photo upload
+    Button uploadPhoto;
+    Button selectPhoto;
+    ImageView previewPhoto;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    private Uri filePath;
+    private final int PICK_IMAGE_REQUEST = 71;
+    static String imgPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +62,33 @@ public class CreatePostActivity extends AppCompatActivity {
         previewBtn = findViewById(R.id.buttonPreviewPost);
         postZip = findViewById(R.id.editTextZip);
         postAdd = findViewById(R.id.editTextAddress);
+
+        //Photo upload buttons instantiation
+        uploadPhoto = findViewById(R.id.buttonUploadPhoto);
+        previewPhoto = findViewById(R.id.imageViewPreviewPhoto);
+        selectPhoto = findViewById(R.id.buttonSelectPhoto);
+
+        //Get instances for FirebaseStorage
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+
+        selectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
+        uploadPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+
+            }
+        });
+
+
         //cancel button
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,28 +102,23 @@ public class CreatePostActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (postTitle.getText().toString().trim().length()==0){
+                if (postTitle.getText().toString().trim().length() == 0) {
                     Toast.makeText(CreatePostActivity.this, "Please give your post a title", Toast.LENGTH_SHORT).show();
-                }
-                else if (postDesc.getText().toString().trim().length()==0){
+                } else if (postDesc.getText().toString().trim().length() == 0) {
                     Toast.makeText(CreatePostActivity.this, "Please describe this posting", Toast.LENGTH_SHORT).show();
-                }
-                else if (postPrice.getText().toString().trim().length()==0){
+                } else if (postPrice.getText().toString().trim().length() == 0) {
                     Toast.makeText(CreatePostActivity.this, "Please give a price for this posting", Toast.LENGTH_SHORT).show();
-                }
-                else if (postAdd.getText().toString().trim().length()==0){
+                } else if (postAdd.getText().toString().trim().length() == 0) {
                     Toast.makeText(CreatePostActivity.this, "Please type in the address", Toast.LENGTH_SHORT).show();
-                }
-                else if (postZip.getText().toString().trim().length()==0){
+                } else if (postZip.getText().toString().trim().length() == 0) {
                     Toast.makeText(CreatePostActivity.this, "Please type in the zip code", Toast.LENGTH_SHORT).show();
-                }
-                else{
+                } else {
                     String title = postTitle.getText().toString();
                     String desc = postDesc.getText().toString();
                     double price = 0;
                     try {
                         price = Double.parseDouble(postPrice.getText().toString());
-                    }catch(Exception ex){
+                    } catch (Exception ex) {
                         Toast.makeText(CreatePostActivity.this, "No price found", Toast.LENGTH_SHORT).show();
                     }
                     int index = category.getSelectedItemPosition();
@@ -77,7 +127,7 @@ public class CreatePostActivity extends AppCompatActivity {
                     String zip = postZip.getText().toString();
                     Intent gotoPreview = new Intent(CreatePostActivity.this, PostPreview.class);
                     //create Bundle
-                    Bundle myBundle =new Bundle();
+                    Bundle myBundle = new Bundle();
                     myBundle.putString("TITLE", title);
                     myBundle.putString("DESC", desc);
                     myBundle.putString("ADD", address);
@@ -85,24 +135,89 @@ public class CreatePostActivity extends AppCompatActivity {
                     myBundle.putInt("CAT_POS", index);
                     myBundle.putString("CAT", cat);
                     myBundle.putDouble("PRICE", price);
+                    myBundle.putString("PHOTO", imgPath);
+                    Log.e("CreatePostActivity", "Bundle " + imgPath);
                     gotoPreview.putExtras(myBundle);
 
-                    startActivityForResult(gotoPreview, LAUNCH_SECOND_ACTIVITY );
+                    startActivityForResult(gotoPreview, LAUNCH_SECOND_ACTIVITY);
                 }
 
             }
         });
     }
+
     @Override
-    public void onActivityResult(int reqCode, int resultCode, Intent data){
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
         super.onActivityResult(reqCode, resultCode, data);
 
         if (reqCode == LAUNCH_SECOND_ACTIVITY) {
-            if(resultCode == Activity.RESULT_OK){
+            if (resultCode == Activity.RESULT_OK) {
                 postTitle.setText(data.getExtras().getString("rtitle"));
                 postDesc.setText(data.getExtras().getString("rdes"));
                 //postPrice.setText(data.getExtras().getString("rprice"));
             }
+        }
+
+        //Show photo preview
+        if (reqCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                previewPhoto.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(CreatePostActivity.this, "Photo selected, please check the preview and select upload.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Photo upload
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    private void uploadImage() {
+
+        if (filePath != null) {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("ddmmyy_HHmmss");
+            String pathComponent = sdf.format(cal.getTime()).toString();
+
+            String imgPathFirebase = "images/" + pathComponent;
+            this.imgPath = imgPathFirebase;
+            StorageReference ref = storageReference.child(imgPathFirebase);
+
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(CreatePostActivity.this, "Photo successfully uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(CreatePostActivity.this, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded " + (int) progress + "%");
+                        }
+                    });
         }
     }
 }
--- app/src/main/java/com/example/seektradeapp/PostPreview.java
+++ app/src/main/java/com/example/seektradeapp/PostPreview.java
@@ -1,17 +1,27 @@
 package com.example.seektradeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
 
 import java.text.SimpleDateFormat;
 
@@ -28,8 +38,13 @@ public class PostPreview extends AppCompatActivity {
     String cat;
     String address;
     String zipcode;
+    String photo;
     FirebaseAuth fAuth;
     DatabaseHelper dbHelper;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    Uri downUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +64,10 @@ public class PostPreview extends AppCompatActivity {
         SimpleDateFormat df = new SimpleDateFormat(("yyyy-MM-dd"), Locale.getDefault());
         String date = df.format(d);
 
+        //Get instances for FirebaseStorage
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
         try {
             price = getIntent().getExtras().getDouble("PRICE");
             cat_pos = getIntent().getExtras().getInt("CAT_POS");
@@ -57,6 +76,7 @@ public class PostPreview extends AppCompatActivity {
             postDesc = getIntent().getExtras().getString("DESC");
             address = getIntent().getExtras().getString("ADD");
             zipcode = getIntent().getExtras().getString("ZIP");
+            photo = getIntent().getExtras().getString("PHOTO");
 
             textViewPostDate.setText(date);
             textViewUser.setText(fAuth.getCurrentUser().getEmail());
@@ -65,6 +85,14 @@ public class PostPreview extends AppCompatActivity {
             textViewPrice.setText("$" + price);
             textViewCategory.setText(cat);
             textViewLocation.setText(address + ", " + zipcode);
+
+
+            StorageReference ref = storageReference.child(photo);
+
+            Log.e("PostPreview", photo);
+            Glide.with(PostPreview.this)
+                    .load(ref)
+                    .into(imageViewPreviewPic);
 
             //back to edit
             backtoEdit.setOnClickListener(new View.OnClickListener() {
@@ -97,14 +125,12 @@ public class PostPreview extends AppCompatActivity {
                     SimpleDateFormat df = new SimpleDateFormat(("yyyy-MM-dd"), Locale.getDefault());
                     String date = df.format(d);
 
-//                    Set photo from FB
-                    String photo = "photo";
                     //String category, String title, String description, double price, int userId, String postDate, String address, String zipCode, String[] photos) {
-                    Post newPost = new Post(cat,postTitle, postDesc, price,  fAuth.getCurrentUser().getEmail(), date, address, zipcode,photo );
+                    Post newPost = new Post(cat, postTitle, postDesc, price, fAuth.getCurrentUser().getEmail(), date, address, zipcode, photo);
 
 
                     //Add Post to the database
-                    dbHelper.addPost(newPost,  fAuth.getCurrentUser().getEmail());
+                    dbHelper.addPost(newPost, fAuth.getCurrentUser().getEmail());
 
                     Intent myIntent = new Intent(PostPreview.this, SearchPostsActivity.class);
                     Bundle myBundle = new Bundle();
