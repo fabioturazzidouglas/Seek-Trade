efd8ab26f40fb8535302cf56915c8409b002010b
Do_Man_Uyen <nguyend42@student.douglascollege.ca>
2020-11-26 22:34:03 -0800

    made final improvemeents to the application

--- app/src/main/java/com/example/seektradeapp/Login.java
+++ app/src/main/java/com/example/seektradeapp/Login.java
@@ -96,51 +96,55 @@ public class Login extends AppCompatActivity {
         DatabaseHelper dbHelper = DatabaseHelper.getInstance(this);
         dbHelper.resetDB();
 
-        List<Post> allActivePosts = new ArrayList<Post>();
-        List<User> allActiveUsers = new ArrayList<User>();
+        List<Post> postCheck = dbHelper.getAllPosts();
 
-        User user1 = new User("xsfsf","Peter", "10/10/2020", "iluvleya@tatooinemail.com");
-        User user2 = new User("xsfsf421", "fabfap", "10/10/2020", "fabfap@gmail.com");
-        User user3 = new User("fadfa32","Admin", "10/10/2020", "admin@admin.com");
-        User user4 = new User("weryr3","Guyn", "10/10/2020", "guyn@hotmail.com");
-        User user5 = new User("fafey5w4","Daniil", "10/10/2020", "denngall2@gmail.com");
-        User user6 = new User("wrqrqw3","Uyen", "10/10/2020", "uyen@gmail.com");
-        User user7 = new User("pw3o4g","Ann-Mary", "10/10/2020", "ann@gmail.com");
+        if (postCheck.size() == 0) {
 
-        allActiveUsers.add(user1);
-        allActiveUsers.add(user2);
-        allActiveUsers.add(user3);
-        allActiveUsers.add(user4);
-        allActiveUsers.add(user5);
-        allActiveUsers.add(user6);
-        allActiveUsers.add(user7);
+            List<Post> allActivePosts = new ArrayList<Post>();
+            List<User> allActiveUsers = new ArrayList<User>();
 
-        for(User user : allActiveUsers) {
-            dbHelper.addOrUpdateUser(user);
+            User user1 = new User("xsfsf", "Peter", "10/10/2020", "iluvleya@tatooinemail.com");
+            User user2 = new User("xsfsf421", "fabfap", "10/10/2020", "fabfap@gmail.com");
+            User user3 = new User("fadfa32", "Admin", "10/10/2020", "admin@admin.com");
+            User user4 = new User("weryr3", "Guyn", "10/10/2020", "guyn@hotmail.com");
+            User user5 = new User("fafey5w4", "Daniil", "10/10/2020", "denngall2@gmail.com");
+            User user6 = new User("wrqrqw3", "Uyen", "10/10/2020", "uyen@gmail.com");
+            User user7 = new User("pw3o4g", "Ann-Mary", "10/10/2020", "ann@gmail.com");
+
+            allActiveUsers.add(user1);
+            allActiveUsers.add(user2);
+            allActiveUsers.add(user3);
+            allActiveUsers.add(user4);
+            allActiveUsers.add(user5);
+            allActiveUsers.add(user6);
+            allActiveUsers.add(user7);
+
+            for (User user : allActiveUsers) {
+                dbHelper.addOrUpdateUser(user);
+            }
+
+            Post post1 = new Post("All Categories", "Brand new Bycicle", "Brand new Bycicle, used for a month and decided to sell because I got another one.", 200, user1.getEmail(), "10/10/2020", "Vancouver, BC", "FAS ASD", "images/bike.jpg");
+            Post post2 = new Post("Vehicles", "Hyundai 2018", "Hyundai in good condition, from 2018. 10000 miles ran.", 15000, user2.getEmail(), "11/11/2010", "New West, BC", "ASD 142", "images/car.jpg");
+            Post post3 = new Post("Real Estate", "House in New Westminster", "1000 sq feet comfortable house with 3 bedroms, 4 bathrooms and backyard.", 900000, user3.getEmail(), "10/10/2020", "New West, BC", "V24 AS2", "images/house.jpg");
+            Post post4 = new Post("Electronics", "Gaming PC", "With GTX 2070, 16GB RAM, 1TB SSD", 700, user4.getEmail(), "11/11/2010", "Surrey, BC", "AS3 2D2", "images/PC.jpg");
+            Post post5 = new Post("All Categories", "Used Bycicle in good condition", "Used Bycicle in perfect condition.", 100, user5.getEmail(), "10/10/2020", "Vancouver, BC", "FAS ASD", "images/bike.jpg");
+            Post post6 = new Post("Electronics", "PS5 - Unique Edition", "PS5, new release by Sony. Comes with one Dualsense controller ", 600, user6.getEmail(), "10/10/2020", "Surrey, BC", "XSA VAS", "images/Ps5.jpg");
+            Post post7 = new Post("Electronics", "New PS5 with controllers and one game", "PS5, new release by Sony. Comes with one Dualsense controller and the new Horizon Zero Dawn 2", 501, user2.getEmail(), "10/10/2020", "Surrey, BC", "XSA VAS", "images/Ps5.jpg");
+            Post post8 = new Post("Electronics", "Used Gaming PC - 2018", "With GTX 1070, 8GB RAM, 1TB SSD", 400, user4.getEmail(), "11/11/2010", "Surrey, BC", "AS3 2D2", "images/PC.jpg");
+            allActivePosts.add(post1);
+            allActivePosts.add(post2);
+            allActivePosts.add(post3);
+            allActivePosts.add(post4);
+            allActivePosts.add(post5);
+            allActivePosts.add(post6);
+            allActivePosts.add(post7);
+            allActivePosts.add(post8);
+
+            for (Post post : allActivePosts) {
+                dbHelper.addPost(post, post.getUserEmail());
+            }
+
+
         }
-
-        Post post1 = new Post("All Categories", "Brand new Bycicle", "Brand new Bycicle, used for a month and decided to sell because I got another one.", 200, user1.getEmail(), "10/10/2020", "Vancouver, BC", "FAS ASD", "images/bike.jpg");
-        Post post2 = new Post("Vehicles", "Hyundai 2018", "Hyundai in good condition, from 2018. 10000 miles ran.", 15000, user2.getEmail(), "11/11/2010", "New West, BC", "ASD 142", "images/car.jpg");
-        Post post3 = new Post("Real Estate", "House in New Westminster", "1000 sq feet comfortable house with 3 bedroms, 4 bathrooms and backyard.", 900000, user3.getEmail(), "10/10/2020", "New West, BC", "V24 AS2", "images/house.jpg");
-        Post post4 = new Post("Electronics", "Gaming PC", "With GTX 2070, 16GB RAM, 1TB SSD", 700, user4.getEmail(), "11/11/2010", "Surrey, BC", "AS3 2D2", "images/PC.jpg");
-        Post post5 = new Post("All Categories", "Used Bycicle in good condition", "Used Bycicle in perfect condition.", 100, user5.getEmail(), "10/10/2020", "Vancouver, BC", "FAS ASD", "images/bike.jpg");
-        Post post6 = new Post("Electronics", "PS5 - Unique Edition", "PS5, new release by Sony. Comes with one Dualsense controller ", 600, user6.getEmail(), "10/10/2020", "Surrey, BC", "XSA VAS", "images/Ps5.jpg");
-        Post post7 = new Post("Electronics", "New PS5 with controllers and one game", "PS5, new release by Sony. Comes with one Dualsense controller and the new Horizon Zero Dawn 2", 501, user2.getEmail(), "10/10/2020", "Surrey, BC", "XSA VAS", "images/Ps5.jpg");
-        Post post8 = new Post("Electronics", "Used Gaming PC - 2018", "With GTX 1070, 8GB RAM, 1TB SSD", 400, user4.getEmail(), "11/11/2010", "Surrey, BC", "AS3 2D2", "images/PC.jpg");
-        allActivePosts.add(post1);
-        allActivePosts.add(post2);
-        allActivePosts.add(post3);
-        allActivePosts.add(post4);
-        allActivePosts.add(post5);
-        allActivePosts.add(post6);
-        allActivePosts.add(post7);
-        allActivePosts.add(post8);
-
-        for (Post post : allActivePosts) {
-            dbHelper.addPost(post, post.getUserEmail());
-        }
-
-
-
     }
 }
